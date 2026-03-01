40ccbb5b801f8dbaea255a16c657b505320d0191
hsleiman2607-dev <h.sleiman2707@gmail.com>
2026-03-01 18:13:11 -0300

    installando cors

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import 'dotenv/config';
 import express from "express";
+import cors from "cors";
 import prisma from "./PrismaClient.js";
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 // ==========================================
 // 1. ROTAS DE CATEGORIAS
 // ==========================================
